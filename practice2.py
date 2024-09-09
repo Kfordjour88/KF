7b92afb327bea8cd5457fbf5e8efb839ecf9aee1
Kwaku Fordjour <kfordjour@live.com>
2024-09-08 23:45:03 -0400

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,12 @@
+while True:
+    x = int(input("How old am I? "))
+    if x == 36:
+        print("Bingo")
+        break
+    else:
+        print("try again!")
+        continue
+        
+    
+
+    
